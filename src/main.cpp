69874e0e7b28e36d1165ae846713bc72a9f40cd1
ReSpecto <evgenixmail@gmail.com>
2021-07-31 14:59:24 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+Web interface & Datalogger for Serial devices
+*/
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
